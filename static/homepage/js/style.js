d447525ee3080d025fbf8167d0044f93902ab282
thanhnhu080195 <34416831+thanhnhu080195@users.noreply.github.com>
2018-04-22 21:42:29 +0700

    command discription

--- static/homepage/js/style.js
+++ static/homepage/js/style.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+/*scrip run tab load size*/
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
