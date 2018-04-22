fecb17dc6cba7a41b8c72a031ce779bdab5a8ad9
nguyenthiennam1992 <nguyenthiennam1992@gmail.com>
2018-04-23 02:34:31 +0700

    test v 6

--- homepage/views.py
+++ homepage/views.py
@@ -5,8 +5,8 @@ from pydrive.drive import GoogleDrive
 # Create your views here.
 def home(request):
     
-    # gauth = GoogleAuth()
-    # gauth.LocalWebserverAuth()
-    # drive = GoogleDrive(gauth)
-    # file_list = drive.ListFile({'q': "'%s' in parents and trashed=false and mimeType='video/mp4'" % '1yYO8HfDvJNcdgLYB-Ms9v4q2VQjUFpHx'}).GetList()
-    return render(request,'homepage/index.html',{'contents':''})
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    file_list = drive.ListFile({'q': "'%s' in parents and trashed=false and mimeType='video/mp4'" % '1yYO8HfDvJNcdgLYB-Ms9v4q2VQjUFpHx'}).GetList()
+    return render(request,'homepage/index.html',{'contents':file_list})
