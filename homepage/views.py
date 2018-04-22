718973c8769dae62bc02fa82cd354121df12b826
nguyenthiennam1992 <nguyenthiennam1992@gmail.com>
2018-04-23 02:17:07 +0700

    fix videos

--- homepage/views.py
+++ homepage/views.py
@@ -8,5 +8,5 @@ def home(request):
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
     drive = GoogleDrive(gauth)
-    file_list = drive.ListFile({'q': "'%s' in parents and trashed=false and mimeType='video/mp4'" % '1cJ0vNiEq2bo_Ui5r2qIfdhASeNTNjCc-'}).GetList()
+    file_list = drive.ListFile({'q': "'%s' in parents and trashed=false and mimeType='video/mp4'" % '1yYO8HfDvJNcdgLYB-Ms9v4q2VQjUFpHx'}).GetList()
     return render(request,'homepage/index.html',{'contents':file_list})
