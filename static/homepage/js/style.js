6b2d54ace55e6f43bcb361431909f097e0e05998
thanhnhu080195 <34416831+thanhnhu080195@users.noreply.github.com>
2018-04-22 17:06:06 +0700

    new

---
+++ static/homepage/js/style.js
@@ -0,0 +1,7 @@
+/*scrip menu click*/
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
