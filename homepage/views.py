191e1f7324460a5613e9c75a9be7e3533ac8d2b9
nguyenthiennam1992 <nguyenthiennam1992@gmail.com>
2018-04-23 23:26:12 +0700

    fix drive ok

--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, HttpResponse
 import urllib.request
-# from pydrive.auth import GoogleAuth
-# from pydrive.drive import GoogleDrive
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
+from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 # Create your views here.
 def home(request):
     gauth = GoogleAuth()
-    auth_url = gauth.CommandLineAuth()
-    return render(request,'homepage/index.html',{'CLIENT_ID':auth_url})
+    gauth.CommandLineAuth()
+    drive = GoogleDrive(gauth) # Create GoogleDrive instance with authenticated GoogleAuth instance
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+        
+    return render(request,'homepage/index.html',{'files':file_list})
